4d106fd13b4e092e562106c895ca8ee9a72c999b
Brittney Nguyen <brittnguyen@gmail.com>
2019-10-08 17:25:40 -0700

    initial folder structure

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+	entry: "./client/index.jsx",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "public")
+	},
+
+	resolve: {
+		extensions: [".jsx", ".js", ".json"]
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.(js | jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}
+			}
+		]
+	},
+	mode: "development"
+};
