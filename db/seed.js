3eb8b7eeba3015d494a85631100447f9d3ff10b6
Brittney Nguyen <brittnguyen@gmail.com>
2019-10-21 16:19:46 -0700

    update to checkout validation logic

--- db/seed.js
+++ db/seed.js
@@ -1,43 +1,44 @@
-var mysql = require('mysql');
-var db = require('./index.js');
-var seedData = require('./seed_data.js');
-var Sequelize = require('sequelize');
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+const db = require('./index.js');
+const seedData = require('./seed_data.js');
 
-/***************** Inserts *****************/
-/*****************************************/
+/** *************** Inserts **************** */
+/** ************************************** */
 
 function insertSeedData(data, table) {
-	var model;
-	if (table === 'listing') {
-		model = db.Listing;
-	} else if (table === 'minmax') {
-		model = db.MinMax;
-	} else if (table === 'bedrooms') {
-		model = db.Bedrooms;
-	} else {
-		model = db.UADays;
-	}
+  seedData.createData();
+  let model;
+  if (table === 'listing') {
+    model = db.Listing;
+  } else if (table === 'minmax') {
+    model = db.MinMax;
+  } else if (table === 'bedrooms') {
+    model = db.Bedrooms;
+  } else {
+    model = db.UADays;
+  }
 
-	Promise.all(
-		data.map(item => {
-			return model
-				.create(item)
-				.then(succ => {
-					console.log('inserted with Sequelize..', table);
-				})
-				.catch(err => {
-					console.log('err inserting with sequilze..', err);
-				});
-		})
-	)
-		.then(res => {
-			console.log('finished inserting seed data for: ', table);
-			//return 'success';
-		})
-		.catch(err => {
-			console.log('error inserting seedData for: ', table);
-			return 'error';
-		});
+  Promise.all(
+    data.map(item => {
+      return model
+        .create(item)
+        .then(succ => {
+          console.log('inserted with Sequelize..', table);
+        })
+        .catch(err => {
+          console.log('err inserting with sequilze..', err);
+        });
+    })
+  )
+    .then(res => {
+      console.log('finished inserting seed data for: ', table);
+      // return 'success';
+    })
+    .catch(err => {
+      console.log('error inserting seedData for: ', table);
+      return 'error';
+    });
 }
 
 insertSeedData(seedData.listing, 'listing');
--- db/seed_data.js
+++ db/seed_data.js
@@ -5,83 +5,82 @@ const listing = [];
 const minmax = [];
 const uaData = [];
 const bedrooms = [];
+function createData() {
+  for (let i = 1; i < 101; i++) {
+    const minmaxData = {
+      sunday_min: Math.floor(Math.random() * 5),
+      monday_min: Math.floor(Math.random() * 3),
+      tuesday_min: Math.floor(Math.random() * 3),
+      wednesday_min: Math.floor(Math.random() * 3),
+      thursday_min: Math.floor(Math.random() * 3),
+      friday_min: Math.floor(Math.random() * 5),
+      saturday_min: Math.floor(Math.random() * 5),
+      max_days: Math.floor(Math.random() * (35 - 7) + 7),
+      listing_id: i
+    };
 
-for (let i = 1; i < 101; i++) {
-  const minmaxData = {
-    sunday_min: Math.floor(Math.random() * 5),
-    monday_min: Math.floor(Math.random() * 3),
-    tuesday_min: Math.floor(Math.random() * 3),
-    wednesday_min: Math.floor(Math.random() * 3),
-    thursday_min: Math.floor(Math.random() * 3),
-    friday_min: Math.floor(Math.random() * 5),
-    saturday_min: Math.floor(Math.random() * 5),
-    max_days: Math.floor(Math.random() * (35 - 7) + 7),
-    listing_id: i
-  };
+    const listingData = {
+      short_desc: faker.lorem.words(Math.floor(Math.random() * 5)),
+      city: faker.address.city(),
+      country: faker.address.country(),
+      discount_rate: (Math.random() * (0.35 - 0.1) + 0.1).toFixed(2),
+      discount_measure: minmaxData.max_days
+    };
 
-  const listingData = {
-    short_desc: faker.lorem.words(Math.floor(Math.random() * 5)),
-    long_desc: faker.lorem.paragraphs(Math.floor(Math.random() * (4 - 1) + 1)),
-    city: faker.address.city(),
-    country: faker.address.country(),
-    discount_rate: (Math.random() * (0.35 - 0.1) + 0.1).toFixed(2),
-    discount_measure: minmaxData.max_days
-    // Math.floor(Math.random() * 35)
-  };
+    listing.push(listingData);
+    minmax.push(minmaxData);
+  }
 
-  listing.push(listingData);
-  minmax.push(minmaxData);
-}
+  // UA Dates
+  for (let x = 1; x < 101; x++) {
+    const numBookedOccurances = Math.floor(Math.random() * (15 - 1) + 1);
 
-// UA Dates
-for (let x = 1; x < 101; x++) {
-  const numBookedOccurances = Math.floor(Math.random() * (15 - 1) + 1);
-
-  for (let z = 0; z < numBookedOccurances; z++) {
-    const totalNumDays = Math.floor(Math.random() * (5 - 2) + 2);
-    const firstDay = faker.date.future(0.5);
-    for (let day = 0; day < totalNumDays; day++) {
-      const data = {
-        ua_date: moment(firstDay)
-          .add(day, 'd')
-          .format('YYYY-MM-DD'),
-        listing_id: x
-      };
-      uaData.push(data);
+    for (let z = 0; z < numBookedOccurances; z++) {
+      const totalNumDays = Math.floor(Math.random() * (5 - 2) + 2);
+      const firstDay = faker.date.future(0.5);
+      for (let day = 0; day < totalNumDays; day++) {
+        const data = {
+          ua_date: moment(firstDay)
+            .add(day, 'd')
+            .format('YYYY-MM-DD'),
+          listing_id: x
+        };
+        uaData.push(data);
+      }
     }
   }
-}
 
-// bedrooms
-for (let x = 1; x < 101; x++) {
-  const numRooms = Math.floor(Math.random() * (6 - 1) + 1);
+  // bedrooms
+  for (let x = 1; x < 101; x++) {
+    const numRooms = Math.floor(Math.random() * (6 - 1) + 1);
 
-  for (let z = 0; z < numRooms; z++) {
-    const totalNumBeds = Math.floor(Math.random() * (4 - 1) + 1);
+    for (let z = 0; z < numRooms; z++) {
+      const totalNumBeds = Math.floor(Math.random() * (4 - 1) + 1);
 
-    if (totalNumBeds === 1) {
-      const bedType = Math.floor(Math.random() * (4 - 1) + 1);
-      let bed;
-      if (bedType === 1) {
-        bed = 'queen';
-      } else if (bedType === 2) {
-        bed = 'double';
-      } else {
-        bed = 'king';
+      if (totalNumBeds === 1) {
+        const bedType = Math.floor(Math.random() * (4 - 1) + 1);
+        let bed;
+        if (bedType === 1) {
+          bed = 'queen';
+        } else if (bedType === 2) {
+          bed = 'double';
+        } else {
+          bed = 'king';
+        }
+        const data = {
+          numBeds: 1,
+          bedType: bed,
+          listing_id: x
+        };
+        bedrooms.push(data);
       }
-      const data = {
-        numBeds: 1,
-        bedType: bed,
-        listing_id: x
-      };
-      bedrooms.push(data);
-    }
 
-    bedrooms.push({
-      numBeds: totalNumBeds,
-      bedType: 'single',
-      listing_id: x
-    });
+      bedrooms.push({
+        numBeds: totalNumBeds,
+        bedType: 'single',
+        listing_id: x
+      });
+    }
   }
 }
 
@@ -118,6 +117,7 @@ uaData = [
 	}]
 */
 
+module.exports.createData = createData;
 module.exports.listing = listing;
 module.exports.minmax = minmax;
 module.exports.uaData = uaData;
