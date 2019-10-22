3c7a255b753dd913518390fe1c084e0a6c776a16
Brittney Nguyen <brittnguyen@gmail.com>
2019-10-21 19:38:43 -0700

    server port number is now 3004

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = require('./router.js');
 
 const app = express();
-const port = 3000;
+const port = 3004;
 
 app.use('/rooms/:id', express.static(path.join(__dirname, '..', 'public')));
 
