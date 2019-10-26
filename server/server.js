12f54a840169ccafb3fd9da54ddfbdb04ab8ce69
Brittney Nguyen <brittnguyen@gmail.com>
2019-10-26 14:50:01 -0700

    compression added to server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const router = require('./router.js');
 
 const app = express();
@@ -10,6 +11,18 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+app.use(compression({ filter: shouldCompress }));
+
+function shouldCompress(req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
 
 app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
